3edddcdcccfa16559e5f49d99e1683a6a30332f3
Le Minh Triet <214134990+giggle-scripter@users.noreply.github.com>
2025-06-08 15:23:22 +0700

    update test

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from llm_support import LLMSupporter, PromptBuilder
 from exact_alg import branch_and_bound
 from evo import ga, llm_ga
 from heuristic import hill_climbing
+from test_generator import generate_randomly_conflict, write_problem
 
 # Load enviroment variables
 load_dotenv()
@@ -21,9 +22,16 @@ llm_supporter = LLMSupporter(model)
 
 random.seed(42)
 
-problem = read_file('test/test_10_many_ads.txt')
+problem = generate_randomly_conflict(25, 120, 0.3, max_budget_type='approx')
+write_problem('test/test_11_very_large_approx_budget.txt',problem)
 
 from co_evo import co_evo_llm
-best, stats = ga(500,100,problem,pm=0.2)
+best, stats = co_evo_llm(800, 100, 16, problem, llm_supporter,
+                             prompt_builder, 
+                             pc=0.8, pm=0.2, elite_ratio=0.1,
+                             heuristic_evo_cycle=50, apply_heuristic_cycle=50,
+                             early_stopping_gen=600, appliable_heuristics=4,
+                             problem_code_filepath='safety_problem_code.txt',
+                             debug=True)
 print(best)
 print(stats)
